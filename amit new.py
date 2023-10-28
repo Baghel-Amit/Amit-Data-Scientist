e585dc4a5459b0e6a02e0303ea2cfa3536dd8346
Baghel-Amit <149222952+Baghel-Amit@users.noreply.github.com>
2023-10-28 17:18:10 +0530

    Add files via upload

---
+++ amit new.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> PRINT("BEGINNER TO MASTER IN DATA SCIENTIST")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    PRINT("BEGINNER TO MASTER IN DATA SCIENTIST")
+NameError: name 'PRINT' is not defined
+>>> print ("BEGINNER TO MASTER IN DATA SCIENTIST")
+BEGINNER TO MASTER IN DATA SCIENTIST
